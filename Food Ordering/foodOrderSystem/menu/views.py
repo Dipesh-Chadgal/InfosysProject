2a2851d5515a1d122799dfee481f5becb8f2f4af
GRISHMA-GEDELA <grishmagedela@gmail.com>
2024-05-30 18:09:19 +0530

    Added to cart

--- Food Ordering/foodOrderSystem/menu/views.py
+++ Food Ordering/foodOrderSystem/menu/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
-from django.contrib.auth import logout,get_user_model
+from django.contrib.auth import get_user_model
 from restaurant.models import foodItems
-# Create your views here.
-from django.shortcuts import render
 
 User = get_user_model()
 
@@ -12,34 +9,23 @@ User = get_user_model()
 def menu(request):
     user = request.user
     foods = foodItems.objects.all()
-    print("my output ",foodItems)
 
     if hasattr(user, 'customeruser'): 
         name = user.customeruser.name 
     else:
-        name = "No name found"      
+        name = "No name found"
 
     if 'cart' not in request.session:
         request.session['cart'] = {}
-    
+
     if request.method == 'POST':
-        
         id = request.POST.get("id")
-        cart = request.session.get('cart')
-        if cart:
-            quantity = cart.get(id)
-            if quantity:
-                cart[id] = quantity+1
-            else:
-                cart[id] = 1
-                
+        cart = request.session.get('cart', {})
+        if id in cart:
+            cart[id] += 1
         else:
-            cart = {}
             cart[id] = 1
         request.session['cart'] = cart
-        
-    
 
-    print('cart',request.session['cart'])
-    return render(request, 'index1.html', {'name': name, 'foodItems':foods})
-
+    print('cart', request.session['cart'])
+    return render(request, 'index1.html', {'name': name, 'foodItems': foods, 'cart': request.session.get('cart', {})})
